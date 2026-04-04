1f6e11987f536eb8e109b5681d131e2be15b3785
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-04 16:14:35 +0530

    Add longestConsecutive function to find sequences
    Implement a solution to find the longest consecutive sequence in a list of integers.

---
+++ week1_arrays_strings_hashing/medium/longest_consecutive_sequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)  # Sorting numbers I think so
+        max_length = 0
+
+        for num in num_set:
+            # Thinking:
+            #This is the beginning as we are checking 2-1 = 1 is there if not then it's set to current
+            if num - 1 not in num_set:
+                current = num
+                length = 1
+            #Continue for other numbers if we found sequence
+                while current + 1 in num_set:
+                    current+=1
+                    length+=1
+
+                max_length = max(max_length, length)
+
+        return max_length
