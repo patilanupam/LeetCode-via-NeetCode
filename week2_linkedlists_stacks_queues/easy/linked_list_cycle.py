10cc9fe982effa01db312f16e026fce16317aa9b
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-06 21:26:32 +0530

    Add cycle detection method in linked list
    Implement Floyd's Cycle detection algorithm for linked lists.

--- week2_linkedlists_stacks_queues/easy/linked_list_cycle.py
+++ week2_linkedlists_stacks_queues/easy/linked_list_cycle.py
@@ -1,2 +1,27 @@
 # Linked List Cycle
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        #FLOYD CYCLE: TORTISE AND HARE APPROACH
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            # As fast traverse one step ahead
+            slow = slow.next
+            fast = fast.next.next
+
+            # If slow and fast meets the cycle exists
+            if slow == fast:
+                return True
+            
+        return False
