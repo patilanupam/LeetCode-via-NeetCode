a41d709927b098d2d8d200c2c7994d4388dadcc3
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-07 23:17:06 +0530

    Add removeElements method to Solution class
    Implement a method to remove elements from a linked list based on a given value.

--- week2_linkedlists_stacks_queues/easy/remove_linked_list_elements.py
+++ week2_linkedlists_stacks_queues/easy/remove_linked_list_elements.py
@@ -1,2 +1,27 @@
 # Remove Linked List Elements
 
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        current = dummy
+
+        # thinking:
+        # always look at next node, not current
+        # so we can "skip" it if needed
+
+        while current.next:
+            if current.next.val == val:
+                # skip the node
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return dummy.next
