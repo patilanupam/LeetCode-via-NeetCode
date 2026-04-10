8d8f0c634b3249f0f8bb65d7d2d1ad8f6566a9fa
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-10 23:52:59 +0530

    Add MyStack class using two queues
    Implemented a stack using two queues with push, pop, top, and empty methods.

--- week2_linkedlists_stacks_queues/easy/implement_stack_using_queues.py
+++ week2_linkedlists_stacks_queues/easy/implement_stack_using_queues.py
@@ -1,2 +1,34 @@
 # Implement Stack using Queues
 
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        #Step 1 Appending element in empty q2
+        self.q2.append(x)
+        #Step 2 Popping the q1 to q2
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+        #Step 3: Exchanging q1 and q2
+        self.q1, self.q2 = self.q2, self.q1   
+
+
+    def pop(self) -> int:
+        return self.q1.popleft()
+
+    def top(self) -> int:
+        return self.q1[0]
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
