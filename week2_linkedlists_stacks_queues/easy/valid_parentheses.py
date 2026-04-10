874583b8f9c8ebba1a5fb90c8bf8b363e7046c66
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-10 23:51:10 +0530

    Add valid parentheses checker using stack
    Implement a function to check for valid parentheses using a stack.

--- week2_linkedlists_stacks_queues/easy/valid_parentheses.py
+++ week2_linkedlists_stacks_queues/easy/valid_parentheses.py
@@ -1,2 +1,26 @@
-# Valid Parentheses
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
 
+        #mapping closing and opening 
+
+        mapping = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+
+        for char in s:
+            #if opening bracket ->push
+            if char in mapping.values():
+                stack.append(char)
+            else:
+             if not stack or stack[-1] != mapping[char]:
+                    return False
+             stack.pop()
+        
+        return len(stack) == 0
