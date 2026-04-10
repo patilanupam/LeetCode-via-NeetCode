9f1932cc758fc3836478ad18d41bf35475c0e1ed
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-10 23:50:36 +0530

    Add palindrome check for linked list
    Implement palindrome check for linked list using Floyd's Tortoise and Hare method to find the middle, reverse the second half, and compare values.

--- week2_linkedlists_stacks_queues/easy/palindrome_linked_list.py
+++ week2_linkedlists_stacks_queues/easy/palindrome_linked_list.py
@@ -1,2 +1,47 @@
 # Palindrome Linked List
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: bool
+        """
+        
+        # Logic same as LinkedList Cycle Floyd Hare TOrtoise method
+        #Step 1  is to find middle element
+        slow= head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+
+        # Reverse Second Half
+        prev = None
+        curr = slow
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        #Step3 Compare both values
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+
+            left = left.next
+            right = right.next
+
+        return True 
+
