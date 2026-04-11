96510dea801d74a9a1af287b6598d7a8ac9150c2
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-11 21:43:01 +0530

    Add MyQueue class to implement queue using stacks
    Implemented a queue using two stacks with methods for push, pop, peek, and empty.

--- week2_linkedlists_stacks_queues/easy/implement_queue_using_stacks.py
+++ week2_linkedlists_stacks_queues/easy/implement_queue_using_stacks.py
@@ -1,2 +1,35 @@
-# Implement Queue using Stacks
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        #Always push into in_stack
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        self._move()
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        self._move()
+        return self.out_stack[-1]
+# We need this in order to move in out_stack based on the elements in in_stack
+    def _move(self) -> int:
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+
+    def empty(self) -> bool:
+        return not self.in_stack and not self.out_stack
+
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()# Implement Queue using Stacks
 
