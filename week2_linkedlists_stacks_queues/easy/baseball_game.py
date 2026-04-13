245d7af872866e1e9e6b1332bc34723fb57888f6
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-13 22:57:06 +0530

    Implement baseball game score calculation

--- week2_linkedlists_stacks_queues/easy/baseball_game.py
+++ week2_linkedlists_stacks_queues/easy/baseball_game.py
@@ -1,2 +1,24 @@
 # Baseball Game
-
+class Solution(object):
+    def calPoints(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        stack = []
+        num = 0
+        total = 0
+        for op in operations:
+            if op == 'C':
+                if stack:
+                    stack.pop()
+            elif op == 'D':
+                num = stack[-1] * 2
+                stack.append(num)
+            elif op == '+':
+                num = stack[-1]+stack[-2]
+                stack.append(num)
+            else:
+                stack.append(int(op))
+        
+        return sum(stack)
