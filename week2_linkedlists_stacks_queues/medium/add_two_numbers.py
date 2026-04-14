141a22b6a8a53a9169daa337d44b36844ecdaad4
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-14 22:45:18 +0530

    Update add_two_numbers.py

--- week2_linkedlists_stacks_queues/medium/add_two_numbers.py
+++ week2_linkedlists_stacks_queues/medium/add_two_numbers.py
@@ -1,2 +1,36 @@
 # Add Two Numbers
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 +carry
+
+            carry = total//10 #Stores 1st digit
+            digit = total%10 #To store actual digit in addition
+
+            current.next = ListNode(digit)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        
+        return dummy.next
+            
+
