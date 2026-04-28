a85348adc395933438b5c18e27c672abc39ca08d
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-28 19:54:15 +0530

    Add dailyTemperatures method to Solution class
    Implement dailyTemperatures method to calculate days until warmer temperatures.

--- week2_linkedlists_stacks_queues/medium/daily_temperatures.py
+++ week2_linkedlists_stacks_queues/medium/daily_temperatures.py
@@ -1,2 +1,14 @@
 # Daily Temperatures
 
+class Solution:
+    def dailyTemperatures(self, temps):
+        results = [0] * len(temps)
+        stack = []
+        # UPVOTE !
+        for i, temp in enumerate(temps):
+            while stack and temps[stack[-1]] < temp:
+                index = stack.pop()
+                results[index] = i - index
+            stack.append(i)
+
+        return results
