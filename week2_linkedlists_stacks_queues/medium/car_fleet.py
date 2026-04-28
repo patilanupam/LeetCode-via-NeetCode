fc07421693e03523f8f8c725a1fedf75dde911da
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-28 19:51:35 +0530

    Add carFleet method to Solution class
    Implement carFleet method to calculate number of car fleets.

--- week2_linkedlists_stacks_queues/medium/car_fleet.py
+++ week2_linkedlists_stacks_queues/medium/car_fleet.py
@@ -1,2 +1,37 @@
 # Car Fleet
 
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+
+        #Pair postion and speed
+        cars = list(zip(position, speed))
+
+        #Sort based on position descending that's why reverse = true
+        cars.sort(reverse = True)
+
+        stack = []
+
+        for pos, spd in cars:
+
+            time = (target - pos) / spd
+
+            #If current car takes longer --> new fleet
+
+            if not stack or time > stack[-1]:
+                stack.append(time)
+
+            #else --> it join existing fleet --> do nothing
+        
+        return len(stack)
+
+
+            
+
+
+        
