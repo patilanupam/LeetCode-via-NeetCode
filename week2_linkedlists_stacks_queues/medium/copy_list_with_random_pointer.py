cc723fbdae1d985096c6e7e325a0e99db81d6f69
Anupam Patil <88701962+patilanupam@users.noreply.github.com>
2026-04-28 19:55:41 +0530

    Add function to copy linked list with random pointers
    Implemented a function to create a deep copy of a linked list with random pointers, maintaining the structure of the original list.

--- week2_linkedlists_stacks_queues/medium/copy_list_with_random_pointer.py
+++ week2_linkedlists_stacks_queues/medium/copy_list_with_random_pointer.py
@@ -1,2 +1,48 @@
 # Copy List with Random Pointer
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head:
+            return None
+        
+        #Step 1: Creating New node for list
+        curr = head
+        while curr:
+            new_node = Node(curr.val)
+            new_node.next = curr.next
+            curr.next = new_node
+            curr = new_node.next
+        
+        #Step 2: Assign Random pointer
+        curr = head
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr= curr.next.next    
+        
+        #Step 3: Seperate Lists
+        curr = head
+        copy_head = head.next
+
+        while curr:
+            copy = curr.next
+            curr.next = copy.next
+            if copy.next:
+                copy.next = copy.next.next
+            curr = curr.next
+        return copy_head
+
+
